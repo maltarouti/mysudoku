d7414fab3546f28ea804bc89204cf04cc2c75fcf
Murtada <murtada.altarouti@gmail.com>
2024-09-25 23:29:10 +0300

    Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='mysudoku',
-    version='1.0',
+    version='1.1',
     author='Murtada Altarouti',
     author_email='murtada.altarouti@gmail.com',
     description='A Sudoku generator and solver',
